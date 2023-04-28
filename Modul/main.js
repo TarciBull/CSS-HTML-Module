bb319e2a0703eccb8bed4eb6f47324ec887a806d
Taras Bulavin <tbul130599@gmail.com>
2023-04-28 13:09:29 +0300

    Initial commit

---
+++ Modul/main.js
@@ -0,0 +1,11 @@
+$(".slick-carousel").slick({
+    //nextArrow: '.next-arrow',
+    //prevArrow: '.prev-arrow',
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    
+  });
